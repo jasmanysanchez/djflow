7cb3eb07d20ea87f6f267b239e5ec3d968af5918
jasmanys <jsanchezm2@unemi.edu.ec>
2020-10-20 22:48:09 -0500

    asd

--- djflow/settings.py
+++ djflow/settings.py
@@ -48,3 +48,9 @@ AUTH_PASSWORD_VALIDATORS = settings['AUTH_PASSWORD_VALIDATORS']
 LOGIN_URL = '/security/login/'
 
 TENANT_MODEL = "tenant.Client"  # modelo tenant.
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- djflow/urls.py
+++ djflow/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from djflow.core.json_settings import get_settings
+from django.views.static import serve
 
 settings_json = get_settings()
 
@@ -10,7 +11,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('djflow.apps.website.urls')),
     url(r'^flow/', include('djflow.apps.flow.urls')),
-    url(r'^security/', include('djflow.apps.security.urls'))
+    url(r'^security/', include('djflow.apps.security.urls')),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings_json["DEBUG"]:
